377fac694a2be49220a896afebe13018d3227ed0
Liuba Kuibida <k2.liuba@gmail.com>
2023-02-03 16:59:50 +0200

    add count helpers for the store

--- src/stores/mood.js
+++ src/stores/mood.js
@@ -1,12 +1,17 @@
-import { ref } from 'vue'
 import { defineStore } from 'pinia'
 import { useStorage } from '@vueuse/core'
 import dayjs from 'dayjs/esm/index.js'
 import utc from 'dayjs/plugin/utc.js'
+import useCounters from '../helpers/store-counters.js'
 
 dayjs.extend(utc)
 
-const today = ref(dayjs())
+const {
+  countRecordsWithUniqueTimestamp,
+  countRecordsWithDifference,
+  countRecordsBeforeToday,
+  countTodayRecords
+} = useCounters()
 
 export const useCalendarStore = defineStore({
   id: 'main',
@@ -20,7 +25,6 @@ export const useCalendarStore = defineStore({
         mood: 'Good'
       }
     ]),
-    todayRecord: {},
     calendar: useStorage('dayList', [])
   }),
   actions: {
@@ -34,54 +38,14 @@ export const useCalendarStore = defineStore({
       const record = [...uniqueRecordsSet.keys()]
       this.totalRecords = [...this.totalRecords, record[0]]
 
-      const recordsWithUniqueTimestamp = this.totalRecords.filter(
-        (record, index, list) => {
-          return (
-            index == list.length - 1 ||
-            list[index + 1].timestamp !== record.timestamp
-          )
-        }
+      const recordsWithUniqueTimestamp = countRecordsWithUniqueTimestamp(
+        this.totalRecords
       )
-
-      const recordsWithDifference = recordsWithUniqueTimestamp.map(
-        (record, index) => {
-          return {
-            ...record,
-            difference:
-              index === 0
-                ? 0
-                : Math.abs(
-                    dayjs(recordsWithUniqueTimestamp[index - 1].date).diff(
-                      dayjs(record.date),
-                      'h'
-                    )
-                  )
-          }
-        }
+      const recordsWithDifference = countRecordsWithDifference(
+        recordsWithUniqueTimestamp
       )
-
-      const recordsBeforeToday = recordsWithDifference.filter(
-        (record, index) => {
-          if (
-            index === 0 ||
-            (record.difference > 0 &&
-              recordsWithUniqueTimestamp[index - 1].date !== record.date &&
-              dayjs(record.date).utc().format('YYYY-MM-DD') !==
-                dayjs(today.value).utc().format('YYYY-MM-DD'))
-          ) {
-            return record
-          }
-        }
-      )
-
-      const todayRecords = recordsWithDifference.filter((record) => {
-        if (
-          dayjs(record.date).utc().format('YYYY-MM-DD') ===
-          dayjs(today.value).utc().format('YYYY-MM-DD')
-        ) {
-          return record
-        }
-      })
+      const recordsBeforeToday = countRecordsBeforeToday(recordsWithDifference)
+      const todayRecords = countTodayRecords(recordsWithDifference)
 
       this.calendar = [
         ...recordsBeforeToday,
